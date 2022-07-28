b1222c61ec294e07d73e77c7f5264ae7424603c1
ikealmighty <isaacyates7@gmail.com>
2022-07-28 12:07:57 -0500

    integrated auth0

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,25 @@
+import { useUser } from "@auth0/nextjs-auth0";
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 export default function Home() {
-  return <div></div>;
+  const { user, error, isLoading } = useUser();
+  const router = useRouter();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>{error.message}</div>;
+
+  if (!user)
+    return (
+      <div>
+        <h1>Welcome to Dating with Friends!</h1>
+        <Link href="/api/auth/login">
+          <a>Click To Login/Signup</a>
+        </Link>
+      </div>
+    );
+  else {
+    return <div>{user.name}</div>;
+  }
 }
--- pages/u/[userId].js
+++ pages/u/[userId].js
@@ -1,3 +1,20 @@
+import { useUser } from "@auth0/nextjs-auth0";
+
 export default function UserProfile() {
-  return <div></div>;
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>{error.message}</div>;
+
+  return (
+    <div>
+      <div>
+        <h1>Date Profile: {user.name}</h1>
+      </div>
+      <div>
+        <h2>Stats:</h2>
+      </div>
+    </div>
+  );
 }
